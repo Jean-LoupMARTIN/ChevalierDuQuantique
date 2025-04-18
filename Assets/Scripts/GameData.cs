5fc12cef6403340470825c319f60ccdd98a604e4
Jean-LoupMARTIN <jlmc78@outlook.fr>
2025-04-18 19:04:37 +0200

    reset

--- Assets/Scripts/GameData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameData", menuName = "Scriptable Objects/GameData")]
-public class GameData : ScriptableObject
-{
-    public float nodesDistance = 1;
-    public float nodesLinkWidth = 0.1f;
-}
